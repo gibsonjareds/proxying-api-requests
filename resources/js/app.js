7371f80fb84b0bf3f20ca7336f7ed7eda90b2845
Jared Gibson <gibsonjareds@gmail.com>
2022-10-02 21:20:10 -0400

    adds some front end setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,8 @@ import { createApp } from 'vue';
 
 const app = createApp({});
 
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
+import AppComponent from './components/AppComponent.vue';
+app.component('app-component', APpComponent);
 
 /**
  * The following block of code may be used to automatically register your
